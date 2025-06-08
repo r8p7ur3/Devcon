cf53d350505d30b011b7dcb0ac1eee25fcbb929b
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2025-06-08 03:33:28 +0100

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,38 @@
 <script lang="ts">
-let { children } = $props();
+	import "../app.css";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+	
+	let { children } = $props();
+	
+	import "../style.css";
 
+  import { Alert } from "flowbite-svelte";
 </script>
-<nav>
-<a href="/"> Home</a>
-<a href="/about"> About Devcon </a>
-<a href="/countdown"> Countdown </a>
+
+<Navbar>
+  <NavBrand href="/">
+    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class=" self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/docs/components/navbar">Navbar</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
 
 
-</nav>
+<div class="p-8">
+<Alert>
+<span class="font-medium">Info alert!</span>
+I need to take a huge shit
+</Alert>
+</div>
 
 {@render children()}
 
 
  
-<style>
-@import './style.css';
-p{
-  color: green;
- }
-
-</style>
