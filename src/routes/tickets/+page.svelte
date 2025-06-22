dd7d427e020e648f4e51b481946f9b02170a4f0b
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2025-06-22 18:11:13 +0100

    emailing done

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -2,7 +2,8 @@
     import pentagon from "$lib/images/devcon-items/pentagon.png"
 
 </script>
-
+<!-- too many thing to do to figure out how to
+ roll out my own tickets atm so this will redirect to eventbrite-->
 <div id="welcome" class="bg-black w-screen h-214.5 relative overflow-hidden border-5 border-black">
 <!-- pentagons-->
     <img class=" absolute opacity-10  top-0 left-[-350px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
@@ -11,14 +12,45 @@
     <img class=" absolute bottom-[-300px] motion-opacity-in-0 opacity-10 left-[1550px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
     <div class="relative top-[100px] motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md -bg white">
     <div class="flex flex-row gap-10 justify-center">
-    <div class=" w-100 h-150 bg-amber-100 rounded-4xl hover:scale-110 duration-600">
-        <button class="transition-all duration-150 ease-in relative cursor-pointer bg-indigo-600 hover:bg-amber-400 rounded-4xl bottom-[-530px] left-[135px] w-30 h-15 ">choose</button>
+    <div class=" w-100 h-150 bg-amber-100 rounded-4xl hover:scale-105 duration-600">
+        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">Free</p>
+        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[140px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€0</button>
+        
+
+        <ul class=" relative bottom-[-200px] left-[40px] font-bold text-wrap align-left *:list-disc">
+            <li>Acsess to all dev con talks online</li>
+            <li>Get emailed on (insert date) with link to event</li>
+        </ul>
+        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
+   
+        <button class="transition-all duration-150  relative cursor-pointer bg-black hover:bg-amber-500 hover:text-black text-white rounded-4xl bottom-[-340px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
     </div>
-    <div class=" relative top-[-30px] w-100 h-170 bg-amber-100 rounded-4xl hover:scale-110 duration-600">
-        <button class="transition-all duration-150  relative cursor-pointer bg-black hover:bg-amber-500 hover:text-black text-white rounded-4xl bottom-[-610px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
+    <div class=" relative top-[-30px] w-100 h-170 bg-amber-100 rounded-4xl hover:scale-105 duration-600">
+        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">in person</p>
+        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[140px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€ 2</button>
+        <ul class=" relative font-bold bottom-[-200px] left-[40px] text-wrap align-left *:list-disc">
+            <li>Acsess to all DevCon talks in person</li>
+            <li>Get emailed on 1st October with link to event</li>
+            <li>Food provided at event</li>
+            <li>Access to recordings of talks</li>
+            <li>Goodie Bag</li>
+        </ul>
+        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
+        <button class="transition-all duration-150  relative cursor-pointer bg-black hover:bg-amber-500 hover:text-black text-white rounded-4xl bottom-[-340px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
     </div>
     <div class=" w-100 h-150 bg-amber-100 rounded-4xl hover:scale-110 duration-600">
-         <button class="transition-all duration-150 ease-in relative cursor-pointer bg-indigo-600 hover:bg-amber-400 rounded-4xl bottom-[-530px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
+        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">online person</p>
+        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[135px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€ 2</button>
+        
+
+        <ul class="relative bottom-[-200px] font-bold left-[40px] text-wrap align-left *:list-disc">
+            <li>Acsess to all DevCon talks online</li>
+            <li>Get emailed on (insert date) with link to event</li>
+            <li>Acsess to recordings of talks</li>
+            <li>Goodie Bag</li>
+        </ul>
+        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
+        <button class="transition-all duration-150 ease-in relative cursor-pointer bg-black text-white hover:text-black hover:bg-amber-400 rounded-4xl bottom-[-290px] left-[135px] w-30 h-15 hover:scale-105">choose</button>
     </div>
     </div>
 </div>
