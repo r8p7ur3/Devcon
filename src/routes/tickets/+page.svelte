626c21f8023b277e6b6a7a34b778a68facfece99
Yakowa <52179180+yakowa@users.noreply.github.com>
2025-06-24 12:09:42 +0100

    Cleaned up Tickets page + added .gitignore

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -13,48 +13,41 @@
     <img class=" absolute  bottom-[-330px] opacity-10 left-[100px]  size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
     <img class=" absolute bottom-[-300px] motion-opacity-in-0 opacity-10 left-[1550px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
     <div class="relative top-[100px] motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md -bg white">
-    <div class="flex flex-colf sm:flex-row gap-10 justify-center">
-    <div class=" w-100 h-150 bg-amber-100 rounded-4xl hover:scale-105 duration-600">
-        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">Free</p>
-        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[140px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€0</button>
-        
+        <div class="flex flex-col lg:flex-row gap-10 justify-center">
+            <a class="flex flex-col justify-start items-center gap-5 w-[95%] lg:w-[25%] bg-[#3f3301] text-slate-300 drop-shadow-2xl rounded-4xl hover:scale-105 duration-600 hover:cursor-pointer p-3" href="javascript:void(0)">
+                <p class="text-center font-extrabold scale-200 text-white">Free</p>
+                <button class="bg-amber-500 w-[60%] rounded-3xl text-center text-3xl font-extrabold scale-110">€ 0</button>
+                <ul class="ml-2 list-disc">
+                    <li>Access to all dev con talks online</li>
+                    <li>Get emailed on (insert date) with link to event</li>
+                </ul>
+            </a>
 
-        <ul class=" relative bottom-[-200px] left-[40px] font-bold text-wrap align-left *:list-disc">
-            <li>Acsess to all dev con talks online</li>
-            <li>Get emailed on (insert date) with link to event</li>
-        </ul>
-        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
-   
-        <button class="transition-all duration-150  relative cursor-pointer bg-black hover:bg-amber-500 hover:text-black text-white rounded-4xl bottom-[-340px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
-    </div>
-    <div class=" relative top-[-30px] w-100 h-170 bg-amber-100 rounded-4xl hover:scale-105 duration-600">
-        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">in person</p>
-        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[140px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€ 2</button>
-        <ul class=" relative font-bold bottom-[-200px] left-[40px] text-wrap align-left *:list-disc">
-            <li>Acsess to all DevCon talks in person</li>
-            <li>Get emailed on 1st October with link to event</li>
-            <li>Food provided at event</li>
-            <li>Access to recordings of talks</li>
-            <li>Goodie Bag</li>
-        </ul>
-        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
-        <button class="transition-all duration-150  relative cursor-pointer bg-black hover:bg-amber-500 hover:text-black text-white rounded-4xl bottom-[-340px] left-[135px] w-30 h-15 hover:scale-110">choose</button>
-    </div>
-    <div class=" w-100 h-150 bg-amber-100 rounded-4xl hover:scale-110 duration-600">
-        <p class="relative text-center bottom-[-50px] font-extrabold scale-200">online person</p>
-        <button class="relative bg-amber-500 h-15 w-30 rounded-3xl left-[135px] bottom-[-100px] text-center text-3xl font-extrabold scale-110">€ 2</button>
-        
+            <a class="flex flex-col justify-start items-center gap-5 w-[95%] lg:w-[25%] bg-[#3f3301] text-slate-300 drop-shadow-2xl rounded-4xl relative top-[-1rem] scale-110 hover:scale-115 duration-600 hover:cursor-pointer p-3" href="javascript:void(0)">
+                <p class="text-center font-extrabold scale-200 text-white">In-Person</p>
+                <button class="bg-amber-500 w-[60%] rounded-3xl text-center text-3xl font-extrabold scale-110">€ 2</button>
+                <ul class="ml-2 list-disc">
+                    <li>Access to <strong>all DevCon talks in person</strong></li>
+                    <li>Get emailed on <strong>October 1st</strong> with link to event</li>
+                    <li><strong>Food provided</strong> at event</li>
+                    <li>Access to <strong>recordings of talks</strong></li>
+                    <li><strong>Goodie Bag</strong></li>
+                </ul>
+            </a>
 
-        <ul class="relative bottom-[-200px] font-bold left-[40px] text-wrap align-left *:list-disc">
-            <li>Acsess to all DevCon talks online</li>
-            <li>Get emailed on (insert date) with link to event</li>
-            <li>Acsess to recordings of talks</li>
-            <li>Goodie Bag</li>
-        </ul>
-        <p class="relative text-sm bottom-[-230px]">*By ordering a ticket, you agree to receive emails informing you about the event. Read the [privacy policy] for more information.</p>
-        <button class="transition-all duration-150 ease-in relative cursor-pointer bg-black text-white hover:text-black hover:bg-amber-400 rounded-4xl bottom-[-290px] left-[135px] w-30 h-15 hover:scale-105">choose</button>
+            <a class="flex flex-col justify-start items-center gap-5 w-[95%] lg:w-[25%] bg-[#3f3301] text-slate-300 drop-shadow-2xl rounded-4xl hover:scale-105 duration-600 hover:cursor-pointer p-3" href="javascript:void(0)">
+                <p class="text-center font-extrabold scale-200 text-white">Online</p>
+                <button class="bg-amber-500 w-[60%] rounded-3xl text-center text-3xl font-extrabold scale-110">€ 2</button>
+                <ul class="ml-2 list-disc">
+                    <li>Access to all <strong>DevCon talks online</strong></li>
+                    <li>Get emailed on <strong>(insert date)</strong> with link to event</li>
+                    <li>Access to <strong>recordings of talks</strong></li>
+                    <li><strong>Goodie Bag</strong></li>
+                </ul>
+            </a>
+        </div>
+
+        <p class="mt-4">By ordering a ticket, you agree to receive emails informing you about the event. Read the <a href="">privacy policy</a> for more information.</p>
     </div>
-    </div>
-</div>
 <!-- second div petagons-->    
 </div>
