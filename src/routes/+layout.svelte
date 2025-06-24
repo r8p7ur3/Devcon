4c47fb6008caa3fd7a9164a71e275deb10914289
Yakowa <52179180+yakowa@users.noreply.github.com>
2025-06-24 17:13:50 +0100

    somewhat fixed nav (still needs mobile support)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,23 +18,21 @@ function OnToggle(){
  <!-- desktop navbar-->
 <ul id="navbar" class="hidden sticky z-10 top-0 md:flex md:space-x-10  bg-black py-6 md:justify-center font-extrabold text-2xl">
     <img class=" absolute opacity-10  top-0 left-[-350px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/">Home</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#About">About</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/countdown">Countdown</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/tickets">Tickets</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/contact">Contact</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/TheTeam">The Team</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#guests">Speakers</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#countdown">Countdown</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#explainer">Whats Happening?</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#tickets">Get Tickets</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#the-team">The Team</a>
   </ul>
 
 <!-- mobile navbar, im thinking hamburger buttone that opens dropdown, need a OnToggle function-->
- <button id="burger" class="flex sticky z-10 top-0 bg-black text-amber-50 md:hidden space-x-10 justify-center w-full text-2xl font-extrabold ">burger</button>
+ <button id="burger" class="flex sticky z-10 top-0 bg-black text-amber-50 md:hidden space-x-10 justify-center w-full text-2xl font-extrabold ">MENU</button>
 <ul id="navbar" class="hidden sticky z-10 top-0 space-x-10  bg-black py-6 md:justify-center font-extrabold text-2xl">
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/">Home</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#About">About</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/countdown">Countdown</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/tickets">Tickets</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/contact">Contact</a>
-    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/TheTeam">The Team</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#guests">Speakers</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#countdown">Countdown</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#explainer">Whats Happening?</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#tickets">Get Tickets</a>
+    <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#the-team">The Team</a>
   </ul>
 
 
