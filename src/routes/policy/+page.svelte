3c3646ca7e623c95269e6ed73113abae07693915
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2025-06-24 20:38:58 +0100

    first iteration of privacy policy

---
+++ src/routes/policy/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+    import pentagon from "$lib/images/devcon-items/pentagon.png"
+
+</script>
+
+
+
+
+
+
+<div class="flex justify-center w-full h-full bg-black relative overflow-hidden">
+  <section id="Policy" class="p-5 max-w-[70%] motion-opacity-in-5 motion-delay-200 flex flex-col justify-center  gap-8 bg-black w-screen">
+    <h1 class="mt-6 text-white text-center text-6xl font-extrabold">Privacy Policy</h1>
+
+    <h1 class="mt-6 text-white text-center text-5xl font-extrabold">How we use your data</h1>
+
+    <p class="text-xl font-bold">We use your data to provide the service of Devcon to you. 
+       We will never use your personally-identifiable information for any use outside of this service. 
+       We will only use your data for the purposes specified in this privacy policy. 
+       We will not sell or rent your data to third parties.</p>
+
+    <p class="text-xl font-bold">We will only use your data to send you emails regarding ordering of tickets, collection of goodiebags and any major updates regarding the event. 
+       We will not use your personal information for marketing or any other such purposes.</p>
+
+    <p class="text-xl font-bold">
+       After the event, we will remove all personal information from our systems that were used with the intention of Devcon 
+       as it will no longer be necessary to us, and we will not use your data for profit.
+    </p>
+
+    <h1 class="mt-6 text-white text-center text-5xl font-extrabold">How we protect your data</h1>
+    <p class="text-xl font-bold">We use a variety of security measures to protect your data, including encryption and firewalls. 
+        We cannot guarantee that your data is safe or secure, however, we will always attempt to protect your data.</p>
+    
+    <h1 class="mt-6 text-white text-center text-5xl font-extrabold">How we store your data</h1>
+    <p class="text-xl font-bold">We maintain transactional logs of payments through evenbrite, who will be responsible for handling your payment information. 
+       We use this service to process payments, as well as tickets. 
+       We do not store your payment information on our servers.
+       We may however keep a record of emails that have been registered to transactions for the purpose of 
+       verificying individuals to come to collect goodiebags at subsequent netsoc events</p>
+
+    <h1 class="mt-6 text-white text-center text-5xl font-extrabold">Changes to this privacy policy</h1>
+    <p class="text-xl font-bold">We may update this privacy policy from time to time. 
+       We will notify you of any changes by posting the new privacy policy on this page. 
+       You are responsible for regularly reviewing this privacy policy to stay informed of any changes.</p>
+
+       <p>If you have any questions about this privacy policy, contact us at <a href="mailto:netsoc@uccsocieties.ie">netsoc@uccsocieties.ie</a></p>
+    
+    
+
+
+    
+
+
+
+   
+  </section>
+
+
+   <img class=" absolute opacity-10  top-0 left-[-350px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
+   <img class=" absolute  top-[-330px] opacity-10 right-[210px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
+   <img class=" absolute  bottom-[-330px] opacity-10 left-[100px]  size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
+   <img class=" absolute bottom-[-300px] motion-opacity-in-0 opacity-10 left-[1550px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon"> 
+</div>
