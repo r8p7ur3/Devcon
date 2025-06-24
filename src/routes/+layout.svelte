4a6a7e00c7b02a6cf3cae6e3d634f7126fa56eaa
Yakowa <->
2025-06-24 12:40:05 +0100

    Removed unnecessary CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import "../app.css";
   import pentagon from "$lib/images/devcon-items/pentagon.png"
 	let { children } = $props();
 
@@ -18,6 +17,7 @@ function OnToggle(){
 <!--nothing here points to anything yet, ignore plz (unless there's something dumb im doing then do point out)-->
  <!-- desktop navbar-->
 <ul id="navbar" class="hidden sticky z-10 top-0 md:flex md:space-x-10  bg-black py-6 md:justify-center font-extrabold text-2xl">
+    <img class=" absolute opacity-10  top-0 left-[-350px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
     <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/">Home</a>
     <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/#About">About</a>
     <a class="transform motion-safe:hover:scale-110 text-white hover:text-orange-600 transition duration-500" href="/countdown">Countdown</a>
