fc2e86621a73ae0adbf675249604b7b85b6d707f
jonathan namanda <50199159+r8p7ur3@users.noreply.github.com>
2025-06-25 21:19:24 +0100

    pentagon fixes & addition of ducks
    moved pentagons that overlapped with buttoms on desktop preventing buttons from being pressed. added ducks and info within about devcon section. fixed overlapping pentagons in mobile to vanish and only display in desktop

--- src/routes/policy/+page.svelte
+++ src/routes/policy/+page.svelte
@@ -53,7 +53,7 @@
 
 
    <img class=" absolute opacity-10  top-0 left-[-350px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
-   <img class=" absolute  top-[-330px] opacity-10 right-[210px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
+   <img class="max-lg:hidden absolute  top-[-330px] opacity-10 right-[210px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
    <img class=" absolute  bottom-[-330px] opacity-10 left-[100px]  size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon">
    <img class=" absolute bottom-[-300px] motion-opacity-in-0 opacity-10 left-[1550px] size-110 scale-in-7 motion-duration-7000 motion-rotate-loop-360/reset motion-ease-linear" src="{pentagon}" alt="pentagon"> 
 </div>
